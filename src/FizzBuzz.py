59ac7435639f29e55babe2fc6fff1870d1bae22d
megh-pota <meghpota5124@gmail.com>
2024-09-04 11:31:53 +0530

    cover scenario for input multiply by 3 and multiply by 5

--- src/FizzBuzz.py
+++ src/FizzBuzz.py
@@ -1,5 +1,7 @@
 def fizzbuzz(param):
-    if param == 3:
-        return "Fizz"
-    else:
+    if param % 5 == 0 and param % 3 == 0:
+        return "FizzBuzz"
+    elif param % 5 == 0:
         return "Buzz"
+    elif param % 3 == 0:
+        return "Fizz"
--- test/test_FizzBuzz.py
+++ test/test_FizzBuzz.py
@@ -1,10 +1,21 @@
-import pytest
-
 from src.FizzBuzz import fizzbuzz
 
 
 def test_check_input_3():
     assert fizzbuzz(3) == "Fizz"
 
+
 def test_check_input_5():
     assert fizzbuzz(5) == "Buzz"
+
+
+def test_check_input_6():
+    assert fizzbuzz(6) == "Fizz"
+
+
+def test_check_input_10():
+    assert fizzbuzz(10) == "Buzz"
+
+
+def test_check_input_15():
+    assert fizzbuzz(15) == "FizzBuzz"
