1c60559db40692d0c21e19e810249c2a4d7b1ef5
megh-pota <meghpota5124@gmail.com>
2024-09-06 11:12:11 +0530

    StringCalculator:1.cover positive and negative scenario. 2. Add guard clause.

--- src/StringCalculator/StringCalculator.py
+++ src/StringCalculator/StringCalculator.py
@@ -1,12 +1,8 @@
 def Add(stringinput: str):
     if stringinput == "":
         return 0
-    else:
-        sum = 0
-        FirstInput = stringinput.split(sep=',')
-        for x in FirstInput:
-            sum = sum + int(x)
-        return sum
-
-
-Add("1,2")
+    sum = 0
+    FirstInput = stringinput.split(sep=',')
+    for x in FirstInput:
+        sum = sum + int(x)
+    return sum
--- test/StringCalculator/test_StringCalculator.py
+++ test/StringCalculator/test_StringCalculator.py
@@ -11,3 +11,11 @@ def test_First_input_4():
 
 def test_one_and_two_input():
     assert Add("1,2") == 3
+
+
+def test_one_to_nine_input():
+    assert Add("1,2,3,4,5,6,7,8,9") == 45
+
+
+def test_nagative_and_positive_input():
+    assert Add("-1,0,1") == 0
